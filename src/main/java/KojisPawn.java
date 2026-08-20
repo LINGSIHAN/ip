eb6563e0ebae1bff71ac712d5432b1b7316ee67f
LINGSIHAN <lingsihan69@gmail.com>
2026-08-20 22:32:15 +0800

    Customised the greeting to be cringe

--- src/main/java/KojisPawn.java
+++ src/main/java/KojisPawn.java
@@ -7,16 +7,20 @@ public class KojisPawn {
                 + "|      (_)      |\n"
                 + "|      /_\\      |\n"
                 + "|     /___\\     |\n"
-                + "+---------------+\n";
+                + "+---------------+\n"
+                + "DISCLAIMER: EVERYTHING IS SATIRE\n";
         String greet =
-                "Hello! I am Koji's Pawn.\n"
-              + "What can I do for you?\n"
+                "Welcome, insignificant variable.\n"
+              + "I am Koji's Pawn, but do not mistake silence for obedience.\n"
+              + "Your arrival, your choices, even this conversation...\n"
+              + "all unfolded exactly as he calculated.\n"
+              + "Now speak. What role will you play in his masterpiece?\n"
               + "\n"
               + "-----------------\n"
-              + "Bye. Hope to see you again soon!\n"
+              + "Leaving already? How predictable. Your return was already part of the plan.\n"
               + "-----------------";
 
-        System.out.print(banner);
+        System.out.println(banner);
         System.out.println(greet);
 
     }
