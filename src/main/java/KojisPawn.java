352e474c5a877134b9c144936e417e5095484dde
LINGSIHAN <lingsihan69@gmail.com>
2026-08-20 21:47:59 +0800

    Edited banner to display name of chatbot and updated names in README

---
+++ src/main/java/KojisPawn.java
@@ -0,0 +1,13 @@
+public class KojisPawn {
+    public static void main(String[] args) {
+        String banner =
+                  "+---------------+\n"
+                + "|  Koji's Pawn  |\n"
+                + "|       _       |\n"
+                + "|      (_)      |\n"
+                + "|      /_\\      |\n"
+                + "|     /___\\     |\n"
+                + "+---------------+\n";
+        System.out.println(banner);
+    }
+}
