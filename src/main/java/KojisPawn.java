f09c80813aa34793b47f94675eb547cb704fbfee
LINGSIHAN <lingsihan69@gmail.com>
2026-08-20 22:39:22 +0800

    seperated exit message from greeting for future logic

--- src/main/java/KojisPawn.java
+++ src/main/java/KojisPawn.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class KojisPawn {
     public static void main(String[] args) {
         String banner =
@@ -16,12 +17,14 @@ public class KojisPawn {
               + "all unfolded exactly as he calculated.\n"
               + "Now speak. What role will you play in his masterpiece?\n"
               + "\n"
-              + "-----------------\n"
+              + "-----------------\n";
+        String exit = "-----------------\n"
               + "Leaving already? How predictable. Your return was already part of the plan.\n"
               + "-----------------";
 
         System.out.println(banner);
         System.out.println(greet);
+        System.out.println(exit);
 
     }
 }
